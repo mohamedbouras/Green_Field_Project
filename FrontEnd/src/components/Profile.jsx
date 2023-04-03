dd1a99c9a0c1691d5d745e8197778bfdb8cbfc05
Med <hamma-01@live.fr>
2023-04-03 23:58:27 +0100

    front

---
+++ FrontEnd/src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import NaveBaree from './NaveBaree'
+import EFouuter from './EFouuter'
+
+
+const Profile = () => {
+    const user = {
+        
+    }
+  return (
+    <div>
+       <NaveBaree />
+       {/* display user profile here */}
+
+       <EFouuter/>
+    </div>
+  )
+}
+
+export default Profile

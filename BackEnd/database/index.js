0d1c963a62f05e45826ecde718ee26cdb03abd70
Saiffeddine <saiffeddinezouaghi@gmail.com>
2023-04-04 00:05:22 +0100

    done

--- BackEnd/database/index.js
+++ BackEnd/database/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 const conn =mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
+    password: "Saif1234",
     database:'E-education'
 })
 conn.connect((err)=>{

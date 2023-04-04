c142399f22955d72b07d99609fc127a5e5747c07
BOURAS_MOHAMED <medb0748@gmail.com>
2023-04-04 15:21:21 -0700

    hii

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -7,30 +7,14 @@ import EFouuter from "./components/EFouuter.jsx"
 import { useState } from "react"
 import AddCours from "./components/addCours.jsx"
 
-import Container from './components/Containerr.jsx'
+import Containerr from './components/Containerr.jsx'
 import {Link,Routes,Route} from 'react-router-dom'
 import Profile from "./components/Profile.jsx"
 
 function App() {
-const [rout,setRout] = useState('Home')
+
   return (
 
-    <div >
-      <NaveBaree />
-      <Container className="mt-5">
-        <button onClick={()=>{setRout('addCours')}}>Add New Course</button>
-     {rout==='Home' ?
-
-     
-       
-: rout === 'addCours'?<div>
-  <AddCours/>
-</div> : <div></div>}
-
-      </Container>
-
-      <EFouuter/>
-    </div>
 
 
         <Routes>

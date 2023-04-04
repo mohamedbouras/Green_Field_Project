60363b6dc5345bc2d06b8274a54e0f26f90fce93
BOURAS_MOHAMED <medb0748@gmail.com>
2023-04-03 19:42:05 -0700

    hello

---
+++ BackEnd/database/index.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2')
+
+const conn =mysql.createConnection({
+    host: "localhost",
+    user: "mohamed",
+    password: "Ines+est123",
+    database:'E-education'
+})
+conn.connect((err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log('db connected')
+    }
+})
+module.exports = conn

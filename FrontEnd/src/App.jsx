c69d77a91ec07f981e74a2cb03672f2385991c02
Med <hamma-01@live.fr>
2023-04-04 15:05:29 +0100

    confirm merge with some changes of mohamed bouras

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,42 +1,29 @@
 
 import NaveBaree from "./components/NaveBaree.jsx"
-import CoursCard from "./components/CoursCard.jsx"
 import { Container, Col, Row } from "react-bootstrap"
-import SearchBar from "./components/searchBar.jsx"
 import EFouuter from "./components/EFouuter.jsx"
 import { useState } from "react"
 import AddCours from "./components/addCours.jsx"
-
-import Container from './components/Containerr.jsx'
-import {Link,Routes,Route} from 'react-router-dom'
+import Containerr from "./components/Containerr.jsx"
+import { Route, Routes } from 'react-router-dom'
 import Profile from "./components/Profile.jsx"
-
 function App() {
-const [rout,setRout] = useState('Home')
+  const [rout, setRout] = useState('Home')
   return (
 
     <div >
-      <NaveBaree />
-      <Container className="mt-5">
-        <button onClick={()=>{setRout('addCours')}}>Add New Course</button>
-     {rout==='Home' ?
+      <Routes>
 
-     
-       
-: rout === 'addCours'?<div>
-  <AddCours/>
-</div> : <div></div>}
+        <Route path='/' element={<Containerr/>} />
+        <Route path='/addCours' element={<AddCours />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
 
-      </Container>
 
-      <EFouuter/>
     </div>
 
 
-        <Routes>
-          <Route path="/" element={<Container/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-        </Routes>
+
 
   )
 }
